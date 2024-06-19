2b5d4540d57932a12c53584b810403dbba6dd83d
nedutg <nedu.tonygreg@gmail.com>
2024-06-18 17:22:55 -0700

    js file

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const mobile_nav = document.querySelector('.burger_mobile');
+const menu_show = document.getElementById('#fa-show');
+const menu_hide = document.getElementById('fa-hide');
+
+menu_show.addEventListener('click', function(){
+      if(mobile_nav.style.display === 'none'){
+        mobile_nav.style.display = 'block';
+        menu_show.style.display = 'none';
+        menu_hide.style.display = 'block';
+      } else {
+        mobile_nav.style.display = 'none';
+        menu_show.style.display = 'block';
+        menu_hide.style.display = 'none'
+      }
+
+
+});
+
+
+menu_show.addEventListener('click', function(){
+    if(mobile_nav.style.display === 'block'){
+      mobile_nav.style.display = 'none';
+      menu_show.style.display = 'block';
+      menu_hide.style.display = 'none';
+    }
+
+
+});
